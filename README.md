7a5e4a0b3282edd3eab1f77bcfe3e88414fc779c
Anton Gorinenko <gorinenko@vtb.ru>
2023-09-14 16:06:35 +0300

    0.0.6

--- README.md
+++ README.md
@@ -6,29 +6,55 @@
 
 ## Python modules
 
-### mq_misc.publish
+### mq_misc.publish info
 
 ```
 python -m mq_misc.publish  -h
 
-usage: __main__.py [-h] [--message MESSAGE] [--message-file MESSAGE_FILE] --amqp-url AMQP_URL --queue QUEUE [--exchange EXCHANGE] [--exchange_type EXCHANGE_TYPE] [--routing_key ROUTING_KEY]
+usage: __main__.py [-h] [-m MESSAGE] [-f MESSAGE_FILE] -url AMQP_URL
+                   [-q QUEUE] [-e EXCHANGE] [--exchange_type EXCHANGE_TYPE]
+                   [-k ROUTING_KEY] [-to REPLY_TO] [-id CORRELATION_ID] [-w]
 
-If an arg is specified in more than one place, then commandline values override environment variables which override defaults.
+If an arg is specified in more than one place, then commandline values
+override environment variables which override defaults.
 
 optional arguments:
   -h, --help            show this help message and exit
-  --message MESSAGE     The message being sent [env var: RMQ_MESSAGE] (default: None)
-  --message-file MESSAGE_FILE
-                        File with the message [env var: RMQ_MESSAGE_FILE] (default: None)
+  -m MESSAGE, --message MESSAGE
+                        The message being sent [env var: RMQ_MESSAGE]
+                        (default: None)
+  -f MESSAGE_FILE, --message-file MESSAGE_FILE
+                        File with the message [env var: RMQ_MESSAGE_FILE]
+                        (default: None)
+  -to REPLY_TO, --reply_to REPLY_TO
+                        Commonly used to name a callback queue [env var:
+                        RMQ_REPLY_TO] (default: None)
+  -id CORRELATION_ID, --correlation_id CORRELATION_ID
+                        Useful to correlate RPC responses with requests. [env
+                        var: RMQ_CORRELATION_ID] (default: None)
+  -w, --waiting_response
+                        Waiting reply_to response. [env var:
+                        RMQ_WAITING_RESPONSE] (default: False)
 
 Rabbit MQ options:
-  --amqp-url AMQP_URL   URL to use to connect to the rabbitmq [env var: RMQ_AMQP_URL] (default: None)
-  --queue QUEUE         Queue to use to connect to the rabbitmq [env var: RMQ_QUEUE] (default: None)
-  --exchange EXCHANGE   Exchange to use to connect to the rabbitmq [env var: RMQ_EXCHANGE] (default: None)
+  -url AMQP_URL, --amqp-url AMQP_URL
+                        URL to use to connect to the rabbitmq [env var:
+                        RMQ_AMQP_URL] (default: None)
+  -q QUEUE, --queue QUEUE
+                        Queue to use to connect to the rabbitmq [env var:
+                        RMQ_QUEUE] (default: None)
+  -e EXCHANGE, --exchange EXCHANGE
+                        Exchange to use to connect to the rabbitmq [env var:
+                        RMQ_EXCHANGE] (default: None)
   --exchange_type EXCHANGE_TYPE
-                        Exchange type to use to connect to the rabbitmq [env var: RMQ_EXCHANGE_TYPE] (default: direct)
-  --routing_key ROUTING_KEY
-                        Routing key for publish message [env var: RMQ_ROUTING_KEY] (default: None)
+                        Exchange type to use to connect to the rabbitmq [env
+                        var: RMQ_EXCHANGE_TYPE] (default: direct)
+  -k ROUTING_KEY, --routing_key ROUTING_KEY
+                        Routing key for publish message [env var:
+                        RMQ_ROUTING_KEY] (default: None)
+
 
 ```
-python -m mq_misc.publish  --amqp-url "amqp://guest:guest@localhost:5672/" --exchange "exchange_1" --exchange_type "topic" --routing_key "log.error" --message-file "message.json"
+
+python -m mq_misc.publish --amqp-url "amqp://guest:guest@localhost:5672/" --exchange "exchange_1" --exchange_type "
+topic" --routing_key "log.error" --message-file "message.json"
--- mq_misc/publish/__main__.py
+++ mq_misc/publish/__main__.py
@@ -1,44 +1,97 @@
 import argparse
 import asyncio
 import logging
+import pprint
+import uuid
 from typing import Optional
 
+import aio_pika
+from aio_pika import DeliveryMode
 from configargparse import ArgumentParser
 
-from mq_misc.amqp import Publisher
+from mq_misc.amqp import create_weak_publisher, ReplyToConsumer
 
 parser = ArgumentParser(
     auto_env_var_prefix="RMQ_", allow_abbrev=False,
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
-parser.add_argument("--message", required=False, help='The message being sent')
-parser.add_argument("--message-file", required=False, help='File with the message')
+parser.add_argument('-m', '--message', required=False, help='The message being sent')
+parser.add_argument('-f', '--message-file', required=False, help='File with the message')
 
 group = parser.add_argument_group('Rabbit MQ options')
-group.add_argument('--amqp-url', required=True, help='URL to use to connect to the rabbitmq')
-group.add_argument('--queue', required=False, help='Queue to use to connect to the rabbitmq')
-group.add_argument('--exchange', required=False, help='Exchange to use to connect to the rabbitmq')
-group.add_argument('--exchange_type', required=False, help='Exchange type to use to connect to the rabbitmq', default='direct')
-group.add_argument('--routing_key', required=False, help='Routing key for publish message')
+group.add_argument('-url', '--amqp-url', required=True, help='URL to use to connect to the rabbitmq')
+group.add_argument('-q', '--queue', required=False, help='Queue to use to connect to the rabbitmq')
+group.add_argument('-e', '--exchange', required=False, help='Exchange to use to connect to the rabbitmq')
+group.add_argument('--exchange_type', required=False, help='Exchange type to use to connect to the rabbitmq',
+                   default='direct')
+group.add_argument('-k', '--routing_key', required=False, help='Routing key for publish message')
+parser.add_argument('-to', '--reply_to', help='Commonly used to name a callback queue', required=False)
+parser.add_argument('-id', '--correlation_id', help='Useful to correlate RPC responses with requests.', required=False)
+parser.add_argument('-w', '--waiting_response', action='store_true', help='Waiting reply_to response.')
 
-logger = logging.getLogger("PUBLISH MESSAGE")
+logger = logging.getLogger('PUBLISH MESSAGE')
 
 
-async def async_main(publisher: Publisher, message: str, routing_key: Optional[str]):
-    logger.info("Connecting...")
-    await publisher.create_connection(robust=False)
+class SimpleReplyToConsumer(ReplyToConsumer):
+    async def process_message(self, body: dict, raw_message: aio_pika.IncomingMessage) -> None:
+        logger.info('Response...\n%s', pprint.pformat(body))
 
-    if publisher.exchange_name:
-        await publisher.declare_exchange(durable=True)
 
-    logger.info("Publish message...")
+async def async_main(args):
+    try:
+        url = args.amqp_url
+        queue = args.queue
+        exchange = args.exchange
+        exchange_type = args.exchange_type
+        message = args.message
+        routing_key = args.routing_key
+        reply_to = args.reply_to
+        correlation_id = args.correlation_id
+        waiting_response = args.waiting_response
 
-    params = {}
-    if routing_key:
-        params['routing_key'] = routing_key
+        if not message:
+            message_file = args.message_file
+            if not message_file:
+                raise ValueError('Specify message or file')
 
-    await publisher.publish(message, **params)
-    logger.info("OK...")
+            with open(message_file, 'r') as f:
+                message = f.read()
+
+        logger.info('Connecting...')
+        async with create_weak_publisher(url, queue, exchange_name=exchange, exchange_type=exchange_type) as publisher:
+            if publisher.exchange_name:
+                await publisher.declare_exchange(durable=True)
+
+            logger.info('Publish message...')
+
+            params = {
+                'delivery_mode': DeliveryMode.NOT_PERSISTENT
+            }
+            if routing_key:
+                params['routing_key'] = routing_key
+
+            if reply_to:
+                params['reply_to'] = reply_to
+
+            if correlation_id:
+                params['correlation_id'] = correlation_id
+
+            if waiting_response:
+                request_consumer = SimpleReplyToConsumer(url)
+
+                await request_consumer.create_consume_connection(robust=False)
+                try:
+                    await request_consumer.publish(message, publisher)
+                    await asyncio.sleep(3)
+                finally:
+                    await request_consumer.close()
+
+            else:
+                await publisher.publish(message, **params)
+
+        logger.info('OK...')
+    except Exception as ex:
+        logger.exception(ex)
 
 
 def main():
@@ -48,26 +101,7 @@ def main():
 
     loop = asyncio.get_event_loop()
 
-    url = args.amqp_url
-    queue = args.queue
-    exchange = args.exchange
-    exchange_type = args.exchange_type
-    message = args.message
-    routing_key = args.routing_key
-
-    if message is None:
-        message_file = args.message_file
-        with open(message_file, 'r') as f:
-            message = f.read()
-
-    publisher = Publisher(url, queue, exchange_name=exchange, exchange_type=exchange_type, loop=loop)
-
-
-
-    try:
-        loop.run_until_complete(async_main(publisher, message, routing_key=routing_key))
-    finally:
-        loop.run_until_complete(publisher.close())
+    loop.run_until_complete(async_main(args))
 
 
 if __name__ == '__main__':
